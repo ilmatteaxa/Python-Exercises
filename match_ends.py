c5ee59fe1e12520dcc10aa8f2800080a56b502ae
ilmatteaxa <matteo.molinari@partners.axa>
2016-10-04 17:38:28 +0200

    Add files via upload

--- match_ends.py
+++ match_ends.py
@@ -8,13 +8,15 @@ Created on Fri Sep 30 11:44:23 2016
     
 def match_ends(words):
     matching=0
-
+    
+    
     for word in words:
         len_word=len(word)
-        first_letter=word[0]
-        last_letter=word[len_word-1]
-        if len_word>=2 and first_letter==last_letter:
-            matching+=1
+        if len_word>=2:
+            first_letter=word[0]
+            last_letter=word[len_word-1]
+            if len_word>=2 and first_letter==last_letter:
+                matching+=1
     return matching
 
 
